29859d97e2706e94289eb45232a00a84736dd5a2
Noah Lundie <86733783+n-lundie@users.noreply.github.com>
2023-08-26 14:57:17 +0100

    add separate update step to server-init

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [ ! -f /home/ubuntu/init.done ]; then
+if [ ! -f /home/ubuntu/update.done ]; then
   sudo apt update && sudo apt upgrade -y
   sudo apt install expect -y
 
+  touch /home/ubuntu/update.done
+
+  sudo reboot
+elif [ ! -f /home/ubuntu/init.done ]; then
+
   cd /home/ubuntu
 
-  wget https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh
+  wget https://git.io/vpn -O openvpn-install.sh
   sudo chmod +x ./openvpn-install.sh
 
   wget https://raw.githubusercontent.com/n-lundie/openvpn-scripts/main/scripts/ovpn-install.exp -O ovpn-install.exp
   sudo chmod +x ./ovpn-install.exp
 
-  sudo ./install.exp
+  sudo ./ovpn-install.exp
 
   sudo cp /root/client.ovpn ./client.ovpn
 
